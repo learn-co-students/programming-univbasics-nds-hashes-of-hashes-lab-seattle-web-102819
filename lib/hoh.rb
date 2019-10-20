737d1db2eb218a93ccbeda4cc366379bc994dfd8
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-20 17:15:56 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -30,6 +30,33 @@
 #   sub_category: nil
 # }
 
+{
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class",
+      sub_category: {
+        label: "Order",
+        sub_category: {
+          label: "Family",
+          sub_category: {
+            label: "Genus",
+            sub_category: {
+              label: "Species"
+            }
+          }
+        }
+      }
+    }
+  },
+  
+   sub_category: nil
+ }
+}
+
+
+
 def naming_system
   # Remember:
   #  Kingdom
